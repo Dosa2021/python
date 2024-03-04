06e23c825ca9e6fdfcb466b4e5928a32b9d6c009
tatsuya <tatsuya.sugawara@iret.co.jp>
2024-03-05 08:52:43 +0900

    12. f文字列のオプション

--- dot/main.py
+++ dot/main.py
@@ -14,6 +14,19 @@
 
 #--------------------------------------------------
 # 11. ユーザからの入力を受け取る
-num = input("Number? ")
-# print(int(num))
-print(float(num)) # 浮動小数点
+# num = input("Number? ")
+# # print(int(num))
+# print(float(num)) # 浮動小数点
+
+#--------------------------------------------------
+# 12. f文字列のオプション
+
+initial_balance = int(input("initial balance? "))
+RATE = 1.1
+
+# ,区切りになる
+print(f"Year: 0 {initial_balance:,}")
+
+# 小数点1000.00の表記
+print(f"Year: 1 {initial_balance * RATE:.2f}")
+print(f"Year: 2 {initial_balance * RATE * RATE:,.2f}")
