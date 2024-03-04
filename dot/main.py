ab4df85d925669f347b86eb771d746f06f609312
tatsuya <tatsuya.sugawara@iret.co.jp>
2024-03-05 08:40:04 +0900

    11. ユーザからの入力を受け取る

--- dot/main.py
+++ dot/main.py
@@ -1,12 +1,19 @@
-# 文字の連結
-name = 'たつや'
-age = 10
+#--------------------------------------------------
+# 10. 文字の連結
+# name = 'たつや'
+# age = 10
 
-# ①
-print(name + str(age) + '歳だよ')
+# # ①
+# print(name + str(age) + '歳だよ')
 
-# ②
-print('{} {} 歳だよ'.format(name, age))
+# # ②
+# print('{} {} 歳だよ'.format(name, age))
 
-# ③
-print(f'{name}{age}歳だよ')
+# # ③
+# print(f'{name}{age}歳だよ')
+
+#--------------------------------------------------
+# 11. ユーザからの入力を受け取る
+num = input("Number? ")
+# print(int(num))
+print(float(num)) # 浮動小数点
