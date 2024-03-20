0f9097055d57d8e3d51e1ed8c385c66cd2dbc591
tatsuya <tatsuya.sugawara@iret.co.jp>
2024-03-21 08:29:45 +0900

    case文まで

--- dot/main.py
+++ dot/main.py
@@ -18,15 +18,53 @@
 # # print(int(num))
 # print(float(num)) # 浮動小数点
 
+# #--------------------------------------------------
+# # 12. f文字列のオプション
+
+# initial_balance = int(input("initial balance? "))
+# RATE = 1.1
+
+# # ,区切りになる
+# print(f"Year: 0 {initial_balance:,}")
+
+# # 小数点1000.00の表記
+# print(f"Year: 1 {initial_balance * RATE:.2f}")
+# print(f"Year: 2 {initial_balance * RATE * RATE:,.2f}")
+
 #--------------------------------------------------
-# 12. f文字列のオプション
+# 13. if
+# 特になし
 
-initial_balance = int(input("initial balance? "))
-RATE = 1.1
+#--------------------------------------------------
+# 14. if else
+# 特になし
 
-# ,区切りになる
-print(f"Year: 0 {initial_balance:,}")
+#--------------------------------------------------
+# 15. 論理演算子
+# if not
 
-# 小数点1000.00の表記
-print(f"Year: 1 {initial_balance * RATE:.2f}")
-print(f"Year: 2 {initial_balance * RATE * RATE:,.2f}")
+#--------------------------------------------------
+# # 16. elif
+# # 17. match
+
+signal = input("Signal color? ")
+
+# # if signal == "red":
+# #     print("Stop!!!!----------")
+# # elif signal == "yell":
+# #     print("Slow down!-----")
+# # elif signal == "blue" or signal == "green":
+# #     print("Go-----")
+# # else:
+# #     print("Invalid color")
+
+# # python3.10の機能らしい。。。
+match signal:
+    case "red":
+        print("Stop!!!!----!!!!!")
+    case "yellow":
+        print("Slow down!")
+    case "blue" | "green":
+        print("Go!")
+    case _:
+        print("Invalid signal color...")
