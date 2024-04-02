46d8948bf97b0d90fe585d4de9647662a6dbbf16
tatsuya <tatsuya.sugawara@iret.co.jp>
2024-04-03 08:53:31 +0900

    forとf文字列

--- dot/main.py
+++ dot/main.py
@@ -71,15 +71,64 @@
 
 #--------------------------------------------------
 # 18. if else を一行で
-initial_balance = 20_000
-# if float(initial_balance) >= 100000:
-#     RATE = 1.1
-# else:
-#     RATE = 1.01
+# initial_balance = 20_000
+# # if float(initial_balance) >= 100000:
+# #     RATE = 1.1
+# # else:
+# #     RATE = 1.01
+
+# # 上と同じ意味
+# RATE = 1.1 if initial_balance >= 100_000 else 1.01
+
+# print(f"Current rate: {RATE:.2f}")
+# print(f"Year 0: {initial_balance:,.2f}")
+# print(f"Year 1: {initial_balance * RATE:,.2f}")
+
+#--------------------------------------------------
+# 19 match と if
+# initial_balance = 80_000
+# # if float(initial_balance) >= 100_000:
+# #     RATE = 1.1
+# # elif float(initial_balance) >= 80_000:
+# #     RATE = 1.08
+# # elif float(initial_balance) >= 60_000:
+# #     RATE = 1.06
+# # else:
+# #     RATE = 1.01
+
+# match initial_balance:
+#     case n if n >= 100_000:
+#         RATE = 1.1
+#     case n if n >= 80_000:
+#         RATE = 1.08
+#     case n if n >= 60_000:
+#         RATE = 1.06
+#     case _:
+#         RATE = 1.01
+# print(f"Current rate: {RATE:.2f}")
+# print(f"Year 0: {initial_balance:,.2f}")
+# print(f"Year 1: {initial_balance * RATE:,.2f}")
+
+
+#--------------------------------------------------
+# 20 反復処理
+
+# for
+# for i in range(10):
+# for i in range(3, 9):
+# for i in range(3, 9, 2): # 3, 5, 7
+# for i in range(8, 5, -1): # 8, 7, 6
+#     print(f"{i} hello")
+
+#--------------------------------------------------
+# 21 forとf文字列
+
+initial_balance = 100_000
+RATE = 1.1
+# print(f"Year 0: {initial_balance * RATE ** 0:,.2f}")
+# print(f"Year 1: {initial_balance * RATE ** 1:,.2f}")
+# print(f"Year 2: {initial_balance * RATE ** 2:,.2f}")
 
 # 上と同じ意味
-RATE = 1.1 if initial_balance >= 100_000 else 1.01
-
-print(f"Current rate: {RATE:.2f}")
-print(f"Year 0: {initial_balance:,.2f}")
-print(f"Year 1: {initial_balance * RATE:,.2f}")
+for i in range(3):
+    print(f"Year {i}: {initial_balance * RATE ** i:,.2f}")
