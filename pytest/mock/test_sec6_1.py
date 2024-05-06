dce6ab41e3b6fe21f52e66eeaddea9c574934ddc
tatsuya <tatsuya.sugawara@iret.co.jp>
2024-05-06 16:32:59 +0900

    GoogleTranslatorクラスのテスト作成

---
+++ pytest/mock/test_sec6_1.py
@@ -0,0 +1,13 @@
+from translator import GoogleTranslator
+import pytest
+
+@pytest.fixture(scope='module')
+def trans():
+    t = GoogleTranslator()
+    print("Create Translator")
+    return t
+
+
+def test_japanese_to_english(trans):
+    test_translated = trans.convert("俺はジャイアン", "日本語", "英語")
+    assert test_translated == "My Name Is Sato."
---
+++ pytest/mock/translator.py
@@ -0,0 +1,12 @@
+class GoogleTranslator():
+    def convert(self, text_original, language_original_name, language_translator_name):
+        print('convert')
+        print(text_original)
+        print(language_original_name)
+        print(language_translator_name)
+        return "My Name Is Sato."
+
+# gt = GoogleTranslator()
+# res = gt.convert("俺はジャイアン", "日本語", "英語")
+# print('r--------------')
+# print(res)
