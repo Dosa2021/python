8ac46c552fbd17f0724a0168adbc0a23f1f354e9
tatsuya <tatsuya.sugawara@iret.co.jp>
2024-05-06 20:07:11 +0900

    convertメソッドの返り値をモック

--- pytest/mock/test_sec6_1.py
+++ pytest/mock/test_sec6_1.py
@@ -8,10 +8,16 @@ def trans():
     return t
 
 
-def test_japanese_to_english(trans):
-    test_translated = trans.convert("俺はジャイアン", "日本語", "英語")
-    assert test_translated == "My Name Is Sato."
+# def test_japanese_to_english(trans):
+#     test_translated = trans.convert("俺はジャイアン", "日本語", "英語")
+#     assert test_translated == "My Name Is Sato."
 
-def test_english_to_japanese(trans):
-    test_translated = trans.convert("My Name Is Sato.", "英語", "日本語")
-    assert test_translated == "俺はジャイアン"
+# mockerを使用し、GoogleTranslatorクラスのconvertメソッドの返り値をモックする
+def test_japanese_to_english(trans, mocker):
+    mocker.patch("translator.GoogleTranslator.convert", return_value = "hello world!")
+    test_translated = trans.convert("俺はジャイアン", "日本語", "英語")
+    print(test_translated)
+
+# def test_english_to_japanese(trans):
+#     test_translated = trans.convert("My Name Is Sato.", "英語", "日本語")
+#     assert test_translated == "俺はジャイアン"
--- pytest/mock/translator.py
+++ pytest/mock/translator.py
@@ -1,12 +1,13 @@
 class GoogleTranslator():
     def convert(self, text_original, language_original_name, language_translator_name):
-        if language_translator_name == '日本語':
-            return "俺はジャイアン"
-        print('convert')
-        print(text_original)
-        print(language_original_name)
-        print(language_translator_name)
-        return "My Name Is Sato."
+        pass
+        # if language_translator_name == '日本語':
+        #     return "俺はジャイアン"
+        # print('convert')
+        # print(text_original)
+        # print(language_original_name)
+        # print(language_translator_name)
+        # return "My Name Is Sato."
 
 # gt = GoogleTranslator()
 # res = gt.convert("俺はジャイアン", "日本語", "英語")
