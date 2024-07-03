0c0fd7a7aeb7d2885d62ebefa2f2cc578cc13008
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-07-03 09:15:56 +0900

    Translatorモジュールの修正

--- pytest/mock/translator.py
+++ pytest/mock/translator.py
@@ -1,22 +1,23 @@
+from googletrans import Translator
+
 class GoogleTranslator():
+    def __init__(self):
+        self.translator = Translator()
+
     def get_language_id(self, language_name):
-        pass
-        # languages = {
-        #     '日本語': 'ja',
-        #     '英語': 'en'
-        # }
-        # return languages[language_name]
+        languages = {
+            '日本語': 'ja',
+            '英語': 'en'
+        }
+        return languages[language_name]
 
     def convert(self, text_original, language_original_name, language_translator_name):
-        language = self.get_language_id(language_translator_name)
+        language_original_id = self.get_language_id(language_original_name)
+        language_translator_id = self.get_language_id(language_translator_name)
+        text_original_translator = self.translator.translate(text_original, dest=language_translator_id)
+        return text_original_translator.text
 
-        if language == 'ja':
-            return "俺はジャイアン"
-
-        return "My Name Is Sato."
-
-
-# gt = GoogleTranslator()
-# res = gt.convert("俺はジャイアン", "日本語", "英語")
-# print('r--------------')
-# print(res)
+gt = GoogleTranslator()
+res = gt.convert("俺はジャイアン", "日本語", "英語")
+print('res-----')
+print(res)
