c7b62dafa88eb4835cb4d6ef0ddadeed99f6502d
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-07-11 08:40:13 +0900

    get_language_idメソッドをmock

--- pytest/mock/test_translator.py
+++ pytest/mock/test_translator.py
@@ -10,4 +10,11 @@ def trans():
 def test_japanese_to_english(trans, mocker):
     mocker.patch("translator.GoogleTranslator.convert", return_value = "hello")
     test_translated = trans.convert("俺はジャイアン", "日本語", "英語") 
+    print('test-1-----')
+    print(test_translated)
+
+def test_english_to_japanese(trans, mocker):
+    mocker.patch("translator.GoogleTranslator.get_language_id", return_value = "ja")
+    test_translated = trans.convert("俺はジャイアン", "日本語", "英語") 
+    print('test-2-----')
     print(test_translated)
